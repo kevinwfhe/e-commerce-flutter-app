f3e1ef741e3454776f704c9138eee367ce843d7c
kevin-7755 <valarmorghulis_kh@outlook.com>
2022-02-10 20:17:20 -0500

    feat(): add ShippingAddress data model and component

--- lib/models/Order.dart
+++ lib/models/Order.dart
@@ -1,14 +1,20 @@
 import 'package:csi5112group1project/models/Product.dart';
 import 'package:csi5112group1project/utils/sharedEnum.dart';
+import 'shipping_address.dart';
 
 class Order {
+  final String orderId;
   final Map<int, int> orderDetails; //Map<product_id, quantity>
   final DateTime orderTimeStamp;
   final ORDER_STATUS orderStatus;
   final double totalPrice;
-  Order(
-      {required this.orderDetails,
-      required this.orderTimeStamp,
-      required this.orderStatus,
-      required this.totalPrice});
+  final ShippingAddress shippingAddress;
+  Order({
+    required this.orderId,
+    required this.orderDetails,
+    required this.orderTimeStamp,
+    required this.orderStatus,
+    required this.totalPrice,
+    required this.shippingAddress,
+  });
 }
---
+++ lib/models/shipping_address.dart
@@ -0,0 +1,11 @@
+class ShippingAddress {
+  final String fullName, phoneNumber, address, city, province, postalCode;
+  ShippingAddress({
+    required this.fullName,
+    required this.phoneNumber,
+    required this.address,
+    required this.city,
+    required this.province,
+    required this.postalCode,
+  });
+}
