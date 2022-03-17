eb6812caa95e29ceb4d4ce2c0d634682323429e2
kevin-7755 <valarmorghulis_kh@outlook.com>
2022-03-17 01:33:50 -0400

    feat(): add http package and get products data from api

--- lib/screens/client/component/body.dart
+++ lib/screens/client/component/body.dart
@@ -5,29 +5,46 @@ import './category.dart';
 import './item_card.dart';
 
 class Body extends StatelessWidget {
+  final Future<List<Product>> products;
+  const Body({Key? key, required this.products}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Category(),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-            child: GridView.builder(
-                itemCount: products.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  mainAxisSpacing: kDefaultPadding,
-                  crossAxisSpacing: kDefaultPadding,
-                  childAspectRatio: 0.75,
-                ),
-                itemBuilder: (context, index) => ItemCard(
-                      product: products[index],
-                      press: () {},
-                    )),
-          ),
-        ),
+        FutureBuilder<List>(
+            future: products,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: kDefaultPadding,
+                    ),
+                    child: GridView.builder(
+                      itemCount: snapshot.data!.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                        mainAxisSpacing: kDefaultPadding,
+                        crossAxisSpacing: kDefaultPadding,
+                        childAspectRatio: 0.75,
+                      ),
+                      itemBuilder: (context, index) => ItemCard(
+                        product: snapshot.data![index],
+                        press: () {},
+                      ),
+                    ),
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                print(snapshot.error);
+                return const Text('error');
+              }
+              return const Text('loading');
+            }),
       ],
     );
   }
--- lib/screens/client/products_screen.dart
+++ lib/screens/client/products_screen.dart
@@ -1,13 +1,37 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:http/http.dart' as http;
+import '../../models/product.dart';
 import '../../constants.dart';
 import './component/body.dart';
 import '../../routes/router.gr.dart';
 // flutter pub add flutter_svg
 
-class ProductsScreen extends StatelessWidget {
+class ProductsScreen extends StatefulWidget {
   const ProductsScreen({Key? key}) : super(key: key);
+
+  @override
+  _ProductsScreenState createState() => _ProductsScreenState();
+}
+
+class _ProductsScreenState extends State<ProductsScreen> {
+  late Future<List<Product>> products;
+
+  Future<List<Product>> apitest() async {
+    var url = Uri.parse('http://3.99.187.201/api/Product');
+    var response = await http.get(url);
+    // print('Response status: ${response.statusCode}');
+    var jsonResponse = jsonDecode(response.body) as List;
+    return jsonResponse.map<Product>((p) => Product.fromJson(p)).toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    products = apitest();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +74,7 @@ class ProductsScreen extends StatelessWidget {
           const SizedBox(width: kDefaultPadding / 2)
         ],
       ),
-      body: Body(),
+      body: Body(products: products),
     );
   }
 }
