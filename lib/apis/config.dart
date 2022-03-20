ba359cfafa811c430bba518c6c77ec9e6783f3ec
kevin-7755 <valarmorghulis_kh@outlook.com>
2022-03-19 20:18:32 -0400

    feat(): expose api base url based on environment

--- lib/apis/config.dart
+++ lib/apis/config.dart
@@ -1,2 +1,4 @@
-// use this fixed ip endpoit before load balancer configured
-const BASE_URL = 'http://3.99.187.201/api'; 
+// Define api base url for different environment
+const BASE_URL = bool.fromEnvironment('dart.vm.product')
+    ? 'https://csi5112group1project-service.kevinhe.dev/api'
+    : 'https://localhost:7098/api';
