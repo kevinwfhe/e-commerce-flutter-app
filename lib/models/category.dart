3d6c7b93a4dc90ddbbf671ffcd0ca77c750af371
kevin-7755 <valarmorghulis_kh@outlook.com>
2022-03-19 23:58:40 -0400

    feat(): add Category model

---
+++ lib/models/category.dart
@@ -0,0 +1,15 @@
+class Category {
+  final String id, name;
+  Category({
+    required this.id,
+    required this.name,
+  });
+
+  Category.fromJson(Map<dynamic, dynamic> json)
+      : id = json['id'],
+        name = json['name'];
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name};
+  }
+}
